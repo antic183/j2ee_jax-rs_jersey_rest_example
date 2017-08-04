a4416384e95f018176438fa9a646bce1a95033f1
Antic Marjan <antic83@gmail.com>
2017-08-04 09:06:20 +0200

    rest config
    rest config

---
+++ src/main/java/com/marjan/rest/RestConfig.java
@@ -0,0 +1,10 @@
+package com.marjan.rest;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class RestConfig extends Application {
+
+}
